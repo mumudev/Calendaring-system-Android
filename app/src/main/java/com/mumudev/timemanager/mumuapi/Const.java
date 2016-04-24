29fd535dd1a32d62d35eb4db1c35654f3d472b41
mumudev <mumudev@163.com>
2016-04-24 14:55:54 +0800

    remove test

--- app/src/main/java/com/mumudev/timemanager/mumuapi/Const.java
+++
@@ -1,7 +0,0 @@
-package com.mumudev.timemanager.mumuapi;
-
-/**
- * Created by 木木工程师 on 2016/3/9.
- */
-public class  Const {
-}
--- app/src/main/java/com/mumudev/timemanager/mumuapi/ControllerURL.java
+++
@@ -1,8 +0,0 @@
-package com.mumudev.timemanager.mumuapi;
-
-/**
- * Created by 木木工程师 on 2016/3/9.
- */
-public class ControllerURL {
-    public static String HOST = "http://192.168.31.111:9999";
-}
